afd542cf54e603ef1b59f81b33d0b97538e711f4
Surandharan <surenphoenix13@gmail.com>
2023-07-15 22:11:40 +0530

    Added MongDB URL

--- db/index.js
+++ db/index.js
@@ -1,9 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/review_app", { useNewUrlParser : true, useUnifiedTopology: true}).
-then((result) => {
-    console.log("database is connected successfully");
-})
-.catch((error) => {
-    console.log(error);
-})
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('db is connected!')
+    })
+    .catch((ex) => {
+        console.log('db connection failed: ', ex)
+    })
